d2a79b22b6536436391c388cd2496e16114353fe
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-09-17 06:55:23 -0500

    Add project files.

---
+++ MinimalAPI/Data/CouponStore.cs
@@ -0,0 +1,14 @@
+﻿using MinimalAPI.Models;
+
+namespace MinimalAPI.Data
+{
+    public static class CouponStore
+    {
+        public static List<Coupon> CopuponList = new List<Coupon>
+        {
+            new Coupon { Id= 1, Name = "10OFF", Percent= 10, IsActive= true },
+            new Coupon { Id= 1, Name = "20OFF", Percent= 20, IsActive= false },
+            new Coupon { Id= 1, Name = "30OFF", Percent= 30, IsActive= true }
+        };
+    }
+}
