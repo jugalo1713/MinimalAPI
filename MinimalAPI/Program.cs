88a908fdcbc9e533ec935b00eb6f124ad4819846
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-09-17 14:46:59 -0500

    single object type api response implemented

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,9 +1,11 @@
 using AutoMapper;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using MinimalAPI;
 using MinimalAPI.Data;
 using MinimalAPI.Models;
 using MinimalAPI.Models.DTO;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,27 +30,53 @@ app.UseHttpsRedirection();
 
 app.MapGet("/api/coupons", (ILogger<Program> _logger) =>
 {
-    _logger.Log(LogLevel.Information,  "Hello World");
-    return Results.Ok(CouponStore.CouponList);
-}).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
+    //_logger.Log(LogLevel.Information,  "Hello World");
+    APIResponse response = new();
+    response.Result = CouponStore.CouponList;
+    response.IsSuccess = true;
+    response.StatusCode =HttpStatusCode.OK;
+
+    return Results.Ok(response);
+}).WithName("GetCoupons").Produces<IEnumerable<APIResponse>>(200);
 
 app.MapGet("/api/coupon/{id:int}", (int id) =>
 {
-    return Results.Ok(CouponStore.CouponList.FirstOrDefault(x => x.Id == id));
-}).WithName("GetCoupon").Produces<Coupon>(200);
+    APIResponse response = new();
+    response.Result = CouponStore.CouponList.FirstOrDefault(x => x.Id == id);
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+
+    return Results.Ok(response);
+}).WithName("GetCoupon").Produces<APIResponse>(200);
 
 app.MapPost("/api/coupon", async ([FromBody] CouponCreateDto createCouponDTO, IMapper mapper, IValidator<CouponCreateDto> _validation ) => 
 {
+    APIResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest};
+
+
     var validateResult = await _validation.ValidateAsync(createCouponDTO);
 
-    if (!validateResult.IsValid)
-        return Results.BadRequest(validateResult.Errors.FirstOrDefault().ToString());
+    if (!validateResult.IsValid
+        || string.IsNullOrEmpty(createCouponDTO.Name)
+        || CouponStore.CouponList.FirstOrDefault(x => x?.Name?.ToLower() == createCouponDTO.Name.ToLower()) != null)
+    {
+        response.ErrorMessages.Add(validateResult.Errors.FirstOrDefault().ErrorMessage.ToString());
+        return Results.BadRequest(response);
+    }
+        
 
-    if (string.IsNullOrEmpty(createCouponDTO.Name))
-        return Results.BadRequest("Invalid Id or coupon name");
+    //if (string.IsNullOrEmpty(createCouponDTO.Name))
+    //{
+    //    response.ErrorMessages.Add(validateResult.Errors.FirstOrDefault().ToString());
+    //    return Results.BadRequest(response);
+    //}
+        
 
-    if (CouponStore.CouponList.FirstOrDefault(x => x?.Name?.ToLower() == createCouponDTO.Name.ToLower()) != null)
-        return Results.BadRequest("Coupon name exists");
+    //if (CouponStore.CouponList.FirstOrDefault(x => x?.Name?.ToLower() == createCouponDTO.Name.ToLower()) != null)
+    //{
+    //    response.ErrorMessages.Add(validateResult.Errors.FirstOrDefault().ToString());
+    //    return Results.BadRequest(response);
+    //}
 
     Coupon coupon = mapper.Map<Coupon>(createCouponDTO);
     coupon.Id = CouponStore.CouponList.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
@@ -58,10 +86,15 @@ app.MapPost("/api/coupon", async ([FromBody] CouponCreateDto createCouponDTO, IM
 
     CouponStore.CouponList.Add(coupon);
 
+    response.Result = couponDTO;
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.Created;
+    return Results.Ok(response);
+
     //return Results.Ok(coupon);
     //return Results.Created($"/api/coupon/{coupon.Id}", coupon);
-    return Results.CreatedAtRoute($"GetCoupon", new { id= couponDTO.Id}, couponDTO);
-}).WithName("CreateCoupon").Accepts<CouponCreateDto>("application/json").Produces<Coupon>(201).Produces(400);
+    //return Results.CreatedAtRoute($"GetCoupon", new { id= couponDTO.Id}, couponDTO);
+}).WithName("CreateCoupon").Accepts<CouponCreateDto>("application/json").Produces<APIResponse>(201).Produces(400);
 
 app.MapPut("/api/coupon", () =>
 {
